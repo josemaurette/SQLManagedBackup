7a8095a769857e4d7a8b843adcc53a0fa5452e99
andrewalumkal <andrew.alumkal@gmail.com>
2021-03-05 11:11:40 -0500

    Update Get-SharedAccessSignature.ps1

--- src/SQLManagedBackup/Get-SharedAccessSignature.ps1
+++ src/SQLManagedBackup/Get-SharedAccessSignature.ps1
@@ -10,7 +10,7 @@
 # Create a new storage account context using an ARM storage account  
 $storageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $AccountKey
 
-$result=Set-AzureStorageContainerStoredAccessPolicy -Container $ContainerName -Policy $PolicyName -Context $storageContext -ExpiryTime ((Get-Date).AddDays(360)) -Permission rwdl
+$result=Set-AzureStorageContainerStoredAccessPolicy -Container $ContainerName -Policy $PolicyName -Context $storageContext -ExpiryTime ((Get-Date).AddDays(730)) -Permission rwdl
 
 # Gets the Shared Access Signature for the policy  
 $sas = New-AzureStorageContainerSASToken -name $ContainerName -Policy $PolicyName -Context $storageContext
